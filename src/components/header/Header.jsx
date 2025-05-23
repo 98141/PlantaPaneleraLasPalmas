558c6acc6404ee1aa4f1bade5236bfddaac780e5
Armando Mora <armandomora14115@gmail.com>
2025-05-22 19:30:27 -0500

    Complementando el header, con rutass funcionales y separas, para un codigo mas limpio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import "./header.css";
+import "../../../blocks/header.css";
+import NavBar from '../navBar/NavBar';
 
 export default function Header() {
-      const customClassName = ({ isActive }) =>
-    "menu__link" + (isActive ? " menu__link_active" : "");
+
     return (
         <header className="header">
             <div className="header__container">
+                <img className="header__logo" src="../../../images/logo_3.webp" alt="Logo Planta Panelera las Palmas" />
                 <h1 className="header__title">Planta Panelera las Palmas</h1>
-                <nav className="header__nav">
-                    <ul className="header__list">
-                        <li className="header__item"><NavLink to="/" className={customClassName}>About</NavLink></li>
-                        <li className="header__item"><NavLink to="/contact" className={customClassName}>Contact</NavLink></li>
-                        <li className="header__item"><NavLink to="/information" className={customClassName}>Information</NavLink></li>
-
-                    </ul>
-                </nav>
+                <NavBar/>
             </div>
         </header>
     )
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,61 @@
+import { NavLink } from "react-router-dom";
+import "../../../blocks/navBar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              "menu__link" + (isActive ? " menu__link_active" : "")
+            }
+          >
+            Inicio
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/empresa"
+            className={({ isActive }) =>
+              "menu__link" + (isActive ? " menu__link_active" : "")
+            }
+          >
+            Empresa
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/products"
+            className={({ isActive }) =>
+              "menu__link" + (isActive ? " menu__link_active" : "")
+            }
+          >
+            Productos
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/videos"
+            className={({ isActive }) =>
+              "menu__link" + (isActive ? " menu__link_active" : "")
+            }
+          >
+            Videos
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              "menu__link" + (isActive ? " menu__link_active" : "")
+            }
+          >
+            Contacto
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
