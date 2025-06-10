5f28b0471045bc23de408e908de460f33b21bb2e
Armando Mora <armandomora14115@gmail.com>
2025-06-10 17:35:07 -0500

    Agregando carrusel al about de main

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,10 @@
+import Carrusel from "./carrusel/carrusel";
+
 export default function About() {
-    return (
-        <div>
-        <h1 className="about__title">Bienvenidos a Planta Panelera Las Palmas</h1>
-        <p className="about__paragraph">
-            Somos una empresa dedicada a la producción y comercialización de panela de alta calidad, ubicada en el corazón de Sandoná, Nariño. Con más de 20 años de experiencia, nos enorgullece ofrecer un producto natural y saludable que endulza la vida de nuestros clientes.
-        </p>
-        
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="about__title">Bienvenidos a Planta Panelera Las Palmas</h1>
+      <Carrusel />
+    </div>
+  );
 }
---
+++ src/components/about/carrusel/carrusel.jsx
@@ -0,0 +1,75 @@
+import { useRef, useEffect, useState } from "react";
+
+const images = [
+  "../../../images/carrusel/imagen_1.jpg",
+  "../../../images/carrusel/imagen_2.jpg",
+  "../../../images/carrusel/imagen_3.jpg",
+  "../../../images/carrusel/imagen_4.jpg",
+  "../../../images/carrusel/imagen_5.jpg",
+  "../../../images/carrusel/imagen_6.jpg",
+];
+
+function Carrusel() {
+  const carouselRef = useRef(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const cellCount = images.length;
+  const theta = 360 / cellCount;
+  const radius = 500 / (2 * Math.tan(Math.PI / cellCount)); // ajusta según tamaño
+
+  // Posicionamiento inicial las imagenes en circulo
+  useEffect(() => {
+    const carousel = carouselRef.current;
+    const cells = carousel.children;
+    for (let i = 0; i < cellCount; i++) {
+      const angle = theta * i;
+      cells[i].style.transform = `rotateY(${angle}deg) translateZ(${radius}px)`;
+    }
+  }, [cellCount, theta, radius]);
+
+  const rotateCarousel = (direction) => {
+    const newIndex = selectedIndex + direction;
+    setSelectedIndex(newIndex);
+    const angle = theta * newIndex * -1;
+    carouselRef.current.style.transform = `translateZ(-${radius}px) rotateY(${angle}deg)`;
+  };
+
+    // Rotar automáticamente
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSelectedIndex((prevIndex) => {
+        const newIndex = prevIndex + 1;
+        const angle = theta * newIndex * -1;
+        carouselRef.current.style.transform = `translateZ(-${radius}px) rotateY(${angle}deg)`;
+        return newIndex;
+      });
+    }, 5000); // cada 5 segundos
+
+     return () => clearInterval(interval); // limpieza al desmontar
+  }, [theta, radius]);
+
+  return (
+
+
+      <div className="carousel__wrapper">
+        <div className="scene">
+          <div className="carousel" ref={carouselRef}>
+            {images.map((src, index) => (
+              <img
+                className="carousel__cell"
+                key={index}
+                src={src}
+                alt={`img-${index}`}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="buttons">
+          <button onClick={() => rotateCarousel(-1)}>◀</button>
+          <button onClick={() => rotateCarousel(1)}>▶</button>
+        </div>
+      </div>
+
+  );
+}
+
+export default Carrusel;
