b6c8ffc8d22eff15e5a209d8871a58692c2d2b42
Armando Mora <armandomora14115@gmail.com>
2025-07-29 20:43:16 -0500

    Modificaciones para pruebas en about de las imagenes, completo

--- src/components/about/products/Products.jsx
+++ src/components/about/products/Products.jsx
@@ -1,27 +1,31 @@
 import { Link } from "react-router-dom";
+import mermelada from "../../../../images/imgProoducts/mermelada.jpg";
+import cuadro from "../../../../images/imgProoducts/panela_cuadro.jpg";
+import pulverizada from "../../../../images/imgProoducts/panela_pulverizada.jpg";
+import redonda from "../../../../images/imgProoducts/panela_redonda.jpg";
 
 export default function Products() {
   const images = [
     {
-      src: "../../../../images/imgProoducts/mermelada.jpg",
+      src: mermelada,
       title: "Mermelada con panela",
       description:
         "Un día soleado en el cultivo de caña de azúcar en Sandoná Nariño.",
     },
     {
-      src: "../../../../images/imgProoducts/panela_cuadro.jpg",
+      src: cuadro,
       title: "Panela Cuadrada ",
       description:
         "Extracción  del jugo de la caña en un trapiche.",
     },
     {
-      src: "../../../../images/imgProoducts/panela_pulverizada.jpg",
+      src: pulverizada,
       title: "Panela Pulverizada",
       description:
         "Un día soleado en el cultivo de caña de azúcar.",
     },
     {
-      src: "../../../../images/imgProoducts/panela_redonda.jpg",
+      src: redonda,
       title: "Panela redonda",
       description:
         "Extracción artesanal del jugo de la caña en un trapiche panelero.",
